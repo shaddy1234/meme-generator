311a354dcac22171b9b3d61a4931ad32ba0d7423
Shadrack Kimaau <musyokishadrach1@gmail.com>
2023-11-08 20:13:47 +0300

    explanation

--- meme-generator/src/Components/Header.jsx
+++ meme-generator/src/Components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.jpg" 
+                src="./images/troll-face.jpg" //image works while on public folder
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
